6c1377828166fadf42eb92bc8edb93694ef21d07
Alessio Balsini <balsini88@yahoo.it>
2013-08-10 20:11:38 +0200

    Completed a basic PC-OBD Elm327 library

--- PC-OBD/ee_elm327.c
+++ PC-OBD/ee_elm327.c
@@ -14,6 +14,76 @@
 char echo_enabled;
 char version[4];
 
+MPR_t MPR[4];
+
+void EE_elm327_MPR_assign(char * mode, char * PID, char byte, MPR_index_t index)
+{
+	MPR[index].mode[0] = mode[0];
+	MPR[index].mode[1] = mode[1];
+	MPR[index].mode[2] = '\0';
+	MPR[index].PID[0] = PID[0];
+	MPR[index].PID[1] = PID[1];
+	MPR[index].PID[2] = '\0';
+	MPR[index].return_byte_num = byte;
+}
+
+void EE_elm327_MPR_init()
+{
+	EE_elm327_MPR_assign("01", "05", 1, ENGINE_COOLANT_TEMP);
+	EE_elm327_MPR_assign("01", "0C", 2, ENGINE_RPM);
+	EE_elm327_MPR_assign("01", "0D", 1, VEHICLE_SPEED);
+	EE_elm327_MPR_assign("01", "11", 1, THROTTLE_POSITION);
+}
+
+int hex_converter(char * hex, char len)
+{
+    int i;
+    int ret = 0;
+    int power = 1;
+
+    for (i=0; i<len; i++) {
+        if (hex[len-i-1] <= 'F' && hex[len-i-1] >= 'A')
+            ret += (hex[len-i-1] - 'A' + 10) * power;
+        else if (hex[len-i-1] <= '9' && hex[len-i-1] >= '0')
+            ret += (hex[len-i-1] - '0') * power;
+        else
+            return -1;
+        power *= 16;
+    }
+
+	return ret;
+}
+
+int ee_elm327_get(MPR_index_t identifier)
+{
+	char return_value[25];
+	int i;
+
+	EE_bluetooth_sendS(MPR[identifier].mode);
+	EE_bluetooth_sendS(" ");
+	EE_bluetooth_sendS(MPR[identifier].PID);
+	EE_bluetooth_sendS("\r\n");
+
+	for (i=0; i<5; i++)
+		EE_bluetooth_receive();
+
+	i=0;
+	for (;;) {
+		return_value[i*2] = EE_bluetooth_receive();
+		return_value[i*2 + 1] = EE_bluetooth_receive();
+		i++;
+		if (i<MPR[identifier].return_byte_num)
+			EE_bluetooth_receive(); // spacing
+		else
+			break;
+	}
+	return_value[i] = '\0';
+
+	while (EE_bluetooth_receive() != '>') ;
+
+	return hex_converter(return_value, MPR[identifier].return_byte_num);
+}
+
 inline int EE_elm327_check_response_no_timeout(char * response)
 {
 	static unsigned char value = 0;
@@ -32,6 +102,7 @@ inline int EE_elm327_check_response_no_timeout(char * response)
 
 void ee_elm327_init()
 {
+	EE_elm327_MPR_init();
 	echo_enabled = 1;
 	ee_elm327_set_echo(0);
 	ee_elm327_reboot();
--- PC-OBD/main.c
+++ PC-OBD/main.c
@@ -14,13 +14,20 @@
 
 int main(int argc, char ** argv)
 {
+	int i;
+
 	EE_bluetooth_init();
 	EE_bluetooth_inquiry();
 	EE_bluetooth_connect();
 
 	ee_elm327_init();
-	printf("Elm327 Version: %s", ee_elm327_get_version());
-
+	printf("Elm327 Version: %s\n", ee_elm327_get_version());
+	for (i=0; i<10; i++) {
+		printf("Throttle: %d\n", ee_elm327_get(THROTTLE_POSITION));
+		printf("RPM: %d\n", ee_elm327_get(ENGINE_COOLANT_TEMP));
+		printf("Coolant Temp: %d\n", ee_elm327_get(ENGINE_RPM));
+		printf("Speed: %d\n", ee_elm327_get(VEHICLE_SPEED));
+	}
 	EE_bluetooth_free();
 	return 0;
 }
