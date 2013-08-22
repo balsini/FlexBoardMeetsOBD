cf35962d981481d96052e1fcf5d91fffc83df734
Alessio Balsini <balsini88@yahoo.it>
2013-08-22 10:56:25 +0200

    Added GUI's skeleton.

---
+++ GUI/main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+    MainWindow main_window;
+    Q_INIT_RESOURCE(images);
+    main_window.show();
+    
+    return application.exec();
+}
