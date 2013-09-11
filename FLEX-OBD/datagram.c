1ad225345cf8e2b3b2c62195b968bb5208a8ee2b
Alessio Balsini <balsini88@yahoo.it>
2013-09-11 19:12:19 +0200

    Renamed and updated files for packets management.

---
+++ FLEX-OBD/datagram.c
@@ -0,0 +1,47 @@
+#include "datagram.h"
+#include "ee_uartusb.h"
+
+void constructDatagram(Datagram * dg,
+		unsigned char type,
+		unsigned char id,
+		unsigned char size,
+		unsigned char * data)
+{
+	dg->type = type;
+	dg->id = id;
+	dg->size = size;
+	dg->data = data;
+}
+
+void sendDatagram(Datagram * datagram)
+{
+	EE_uartusb_sendC(datagram->type);
+	EE_uartusb_sendC(datagram->id);
+	EE_uartusb_sendC(datagram->size);
+	if (datagram->size > 0)
+		EE_uartusb_sendS(datagram->data);
+}
+
+void sendDatagramSmall(unsigned char type, unsigned char id)
+{
+	Datagram dg;
+	dg.type = type;
+	dg.id = id;
+	dg.size = 0;
+	dg.data = 0;
+	sendDatagram(&dg);
+}
+
+void receiveDatagram(Datagram * datagram)
+{
+	int i;
+	datagram->type = EE_uartusb_receive();
+	datagram->id = EE_uartusb_receive();
+	datagram->size = EE_uartusb_receive();
+	if (datagram->size > 0) {
+		for (i=0; i<datagram->size; i++)
+			datagram->data[i] = EE_uartusb_receive();
+	} else {
+		datagram->data = 0;
+	}
+}
