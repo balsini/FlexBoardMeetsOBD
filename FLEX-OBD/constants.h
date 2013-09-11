1a59bb9b60504fbf4207add55c4a26b290060573
Alessio Balsini <balsini88@yahoo.it>
2013-09-11 19:19:53 +0200

    Updated USB_UART constants.

--- FLEX-OBD/constants.h
+++ FLEX-OBD/constants.h
@@ -15,4 +15,9 @@
 #define BT_PARAM		BIT8_NO | BIT_STOP_1
 #define BT_CONG_CTRL	CTRL_SIMPLE
 
+#define USB_UART		1
+#define USB_BAUD_RATE	9600
+#define USB_PARAM		BIT8_NO | BIT_STOP_1
+#define USB_CONG_CTRL	CTRL_SIMPLE
+
 #endif /* CONSTANTS_H_ */
