7cdb13ea4167014145a1f2170348411034f7adae
Alessio Balsini <balsini88@yahoo.it>
2013-09-11 15:15:55 +0200

    Added coolant temperature and fuel remaining.

--- FLEX-OBD-LCD/vehicleData.h
+++ FLEX-OBD-LCD/vehicleData.h
@@ -8,15 +8,20 @@
 #ifndef VEHICLEDATA_H_
 #define VEHICLEDATA_H_
 
-#define VEHICLE_DATA_SIZE		3
-#define VEHICLE_DATA_VERSION	0x00
-#define VEHICLE_DATA_SPEED		0x01
-#define VEHICLE_DATA_RPM		0x02
+#define VEHICLE_DATA_SIZE		5
+#define VEHICLE_DATA_VERSION	0x01
+#define VEHICLE_DATA_SPEED		0x02
+#define VEHICLE_DATA_RPM		0x04
+#define VEHICLE_DATA_COOLANT_TEMP		0x08
+#define VEHICLE_DATA_FUEL		0x16
 
 typedef struct vehicleData_ {
+	unsigned int ready;
 	char Elm327version[4];
 	unsigned int speed;
 	unsigned int RPM;
+	unsigned int coolantTemp;
+	unsigned int fuelRemaining;
 } vehicleData;
 
 extern vehicleData vehicleDataBuffer;
